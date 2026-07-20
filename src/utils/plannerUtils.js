ba3927bc0d943198b4b439e860db502ec59ef978
Nico San Miguel <nico1745sanmiguel@gmail.com>
2026-07-20 14:17:02 -0300

    fix(planner): fix infinite render loop by returning stable default settings reference

--- src/utils/plannerUtils.js
+++ src/utils/plannerUtils.js
@@ -1,14 +1,16 @@
 import { CACHE_KEYS } from '../config/constants';
 import { getCache } from './cache';
 
+const DEFAULT_PLANNER_SETTINGS = {
+    compactView: false,
+    hideCompleted: false,
+    initialState: 'default', // 'default', 'expanded', 'collapsed'
+    sortBy: 'date', // 'date', 'price_desc', 'alpha'
+    budgetAlert: 0
+};
+
 export const getPlannerSettings = () => {
-    return getCache(CACHE_KEYS.PLANNER_SETTINGS) || {
-        compactView: false,
-        hideCompleted: false,
-        initialState: 'default', // 'default', 'expanded', 'collapsed'
-        sortBy: 'date', // 'date', 'price_desc', 'alpha'
-        budgetAlert: 0
-    };
+    return getCache(CACHE_KEYS.PLANNER_SETTINGS) || DEFAULT_PLANNER_SETTINGS;
 };
 
 export const subscribeToPlannerSettings = (callback) => {
