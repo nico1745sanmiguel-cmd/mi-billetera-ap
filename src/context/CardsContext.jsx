d7bd74019e9c7b9bbb94ebbe7f54069d95c19773
Nico San Miguel <nico1745sanmiguel@gmail.com>
2026-09-15 16:11:59 -0300

    fix: remove duplicate uid/householdId declarations in CardsContext left by merge

--- src/context/CardsContext.jsx
+++ src/context/CardsContext.jsx
@@ -45,9 +45,6 @@ export const CardsProvider = ({ children }) => {
     const uid = user?.uid;
     const householdId = userData?.householdId;
 
-    const uid = user?.uid;
-    const householdId = userData?.householdId;
-
     useEffect(() => {
         if (!uid) {
             setLoadingCards(false);
